f490f03190ed8b4b817ef5b5143384c46a6c253e
ВИТАЛИЙ <Vitafanasyev2000@yandex.ru>
2025-11-02 16:05:32 +0300

    Migrate to Jetpack Compose

--- app/src/main/java/com/example/shabasher/MainActivity.kt
+++ app/src/main/java/com/example/shabasher/MainActivity.kt
@@ -1,37 +1,47 @@
 package com.example.shabasher
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.shabasher.ui.theme.ShabasherTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContent {
+            ShabasherTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
         }
+    }
+}
 
-        val regButton = findViewById<Button>(R.id.button_register)
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
 
-        regButton.setOnClickListener {
-            val intent = Intent(this, registrationActivity::class.java)
-            startActivity(intent)
-        }
-
-        val loginButton = findViewById<Button>(R.id.button_login)
-
-        loginButton.setOnClickListener {
-            val intent = Intent(this, loginActivity::class.java)
-            startActivity(intent)
-        }
-
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ShabasherTheme {
+        Greeting("Android")
     }
 }
