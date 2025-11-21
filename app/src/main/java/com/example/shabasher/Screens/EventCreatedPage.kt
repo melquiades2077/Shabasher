d8398580d6875b2e19edbb25d19a0b4473158d10
KOCHEG4R <dmitry.moiseenko.06@yandex.ru>
2025-11-21 18:36:55 +0300

    feat: Add new page file

---
+++ app/src/main/java/com/example/shabasher/Screens/EventCreatedPage.kt
@@ -0,0 +1,17 @@
+package com.example.shabasher.Screens
+
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun EventCreated(modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+fun EventCreatedPreview() {
+    EventCreated()
+}
