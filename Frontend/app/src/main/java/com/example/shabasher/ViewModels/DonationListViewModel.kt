97dfb6005a654e4ad1f926435f960681ce41f7b2
KOCHEG4R <dmitry.moiseenko.06@yandex.ru>
2026-02-28 02:41:00 +0300

    add viewmodel prototype

--- Frontend/app/src/main/java/com/example/shabasher/ViewModels/DonationListViewModel.kt
+++ Frontend/app/src/main/java/com/example/shabasher/ViewModels/DonationListViewModel.kt
@@ -1,2 +1,51 @@
 package com.example.shabasher.ViewModels
 
+import androidx.lifecycle.ViewModel
+import com.example.shabasher.Model.Donation
+import com.example.shabasher.Model.DonationPaymentStatus
+import com.example.shabasher.Model.DonationStatus
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class DonationListViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(DonationListState())
+    val uiState: StateFlow<DonationListState> = _uiState
+
+    fun fetchDonations() {
+        val mockDonations = listOf(
+            Donation(
+                title = "Поездка в горы на выходные",
+                description = "Сбор на транспорт и проживание для группы из 12 человек",
+                status = DonationStatus.ACTIVE,
+                paymentStatus = DonationPaymentStatus.PAID,
+                collectedAmount = 13_000,
+                targetAmount = 20_000,
+                paidParticipants = 5,
+                totalParticipants = 12,
+                paymentDetails = "Tinkoff •••• 1234\nАлексей Петров",
+                organizerName = "Алексей Петров"
+            ),
+            Donation(
+                title = "Сорваться к морю",
+                description = "Сбор на транспорт и проживание для группы из 12 человек",
+                status = DonationStatus.CLOSED,
+                paymentStatus = DonationPaymentStatus.NOT_PAID,
+                collectedAmount = 5_000,
+                targetAmount = 30_000,
+                paidParticipants = 10,
+                totalParticipants = 15,
+                paymentDetails = "Tinkoff •••• 1234\nАлексей Петров",
+                organizerName = "Алексей Петров"
+            )
+        )
+        _uiState.value = _uiState.value.copy(donations = mockDonations)
+    }
+
+    init {
+        fetchDonations()
+    }
+}
+
+data class DonationListState(
+    val donations: List<Donation> = emptyList()
+)
