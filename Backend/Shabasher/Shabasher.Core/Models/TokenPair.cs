7fd9d385c0bc86852b2d9eb6435bd51c65a9bc0e
Ярослав Конищев <y.konishchev90@mail.ru>
2026-03-26 14:32:27 +0300

    обновил конфигурацию jwt

---
+++ Backend/Shabasher/Shabasher.Core/Models/TokenPair.cs
@@ -0,0 +1,9 @@
+﻿namespace Shabasher.Core.Models
+{
+    public class TokenPair
+    {
+        public string AccessToken { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime AccessTokenExpiration { get; set; }
+    }
+}
